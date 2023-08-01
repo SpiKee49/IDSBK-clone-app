5ad60b8204e9981655f3627f34392ac3db6e5fad
René Bukovina <r.bukovina@gmail.com>
2023-08-01 20:29:52 +0200

    opening modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,73 +1,93 @@
-import { BsCircle, BsCircleFill } from 'react-icons/bs';
-import { useContext, useState } from 'react';
+import { BsCircle, BsCircleFill } from 'react-icons/bs'
+import { useContext, useEffect, useState } from 'react'
 
-import { IconContext } from 'react-icons';
-import { ModalContext } from '../App';
+import { IconContext } from 'react-icons'
+import { ModalContext } from '../App'
 
 function Modal() {
-  const modalContext = useContext(ModalContext);
-  const items = [
-    'Základné lístky',
-    'Zľavnené lístky',
-    'Denné lístky',
-    'Batožina / pes',
-    'Bratislava - Hainburg',
-  ];
+    const modalContext = useContext(ModalContext)
+    const items = [
+        'Základné lístky',
+        'Zľavnené lístky',
+        'Denné lístky',
+        'Batožina / pes',
+        'Bratislava - Hainburg',
+    ]
 
-  const [selectedItem, setSelectedItem] =
-    useState<number>();
+    const [selectedItem, setSelectedItem] = useState<number>()
+    const [slideModalUp, setSlideModalUp] = useState(false)
+    const [showModal, setShowModal] = useState(false)
 
-  return (
-    <div
-      className={`w-full h-screen z-50 flex flex-col left-0${
-        modalContext?.isModalOpen
-          ? 'absolute top-0'
-          : 'hidden top-full'
-      } absolute bg-black bg-opacity-50 transition-all duration-300 ease-in-out back`}
-    >
-      <div
-        className="w-full h-1/2"
-        onClick={() => modalContext?.setOpenModal(false)}
-      ></div>
-      <div className="rounded-t-3xl h-1/2 w-full flex flex-col gap-4 justify-start items-center bg-white">
-        <span className="rounded-t-3xl bg-primary w-full text-center text-white py-[2px]">
-          Jednorázový cestovný lístok
-        </span>
-        <IconContext.Provider
-          value={{ color: '#00a6e3', size: '25px' }}
+    useEffect(() => {
+        if (modalContext?.isModalOpen) {
+            setShowModal(true)
+            setTimeout(() => {
+                setSlideModalUp(true)
+            }, 100)
+        } else {
+            // if isModalOpen === false
+            setSlideModalUp(false)
+            setTimeout(() => {
+                setShowModal(false)
+            }, 300)
+        }
+    }, [modalContext])
+
+    return (
+        <div
+            className={`w-full h-screen z-[45] bg-black  transition-all duration-500 ease-in-out ${
+                showModal ? 'absolute bg-opacity-50' : 'hidden bg-opacity-0'
+            }`}
         >
-          <div className="flex flex-col w-[85%]">
-            {items.map((item, index) => (
-              <div
-                className="flex flex-row items-center gap-2 h-12"
-                onClick={() => setSelectedItem(index)}
-              >
-                {selectedItem === index ? (
-                  <BsCircleFill />
-                ) : (
-                  <BsCircle />
-                )}
-                {item}
-              </div>
-            ))}
-          </div>
-        </IconContext.Provider>
-        <button
-          type="button"
-          className="w-[90%] h-12 shadow-md rounded-full bg-primary text-white"
-        >
-          Potvrdiť výber
-        </button>
-        <button
-          onClick={() => modalContext?.setOpenModal(false)}
-          type="button"
-          className="w-[90%] h-12 shadow-md rounded-full bg-white text-primary"
-        >
-          Späť
-        </button>
-      </div>
-    </div>
-  );
+            <div
+                className={`w-full h-screen absolute z-50 flex flex-col left-0 ${
+                    slideModalUp ? 'top-0' : 'top-full'
+                }  transition-all duration-500 ease-in-out`}
+            >
+                <div
+                    className="w-full h-1/2"
+                    onClick={() => modalContext?.setOpenModal(false)}
+                ></div>
+                <div className="rounded-t-3xl h-1/2 w-full flex flex-col gap-4 justify-start items-center bg-white">
+                    <span className="rounded-t-3xl bg-primary w-full text-center text-white py-[2px]">
+                        Jednorázový cestovný lístok
+                    </span>
+                    <IconContext.Provider
+                        value={{ color: '#00a6e3', size: '25px' }}
+                    >
+                        <div className="flex flex-col w-[85%]">
+                            {items.map((item, index) => (
+                                <div
+                                    className="flex flex-row items-center gap-2 h-12"
+                                    onClick={() => setSelectedItem(index)}
+                                >
+                                    {selectedItem === index ? (
+                                        <BsCircleFill />
+                                    ) : (
+                                        <BsCircle />
+                                    )}
+                                    {item}
+                                </div>
+                            ))}
+                        </div>
+                    </IconContext.Provider>
+                    <button
+                        type="button"
+                        className="w-[90%] h-12 shadow-md rounded-full bg-primary text-white"
+                    >
+                        Potvrdiť výber
+                    </button>
+                    <button
+                        onClick={() => modalContext?.setOpenModal(false)}
+                        type="button"
+                        className="w-[90%] h-12 shadow-md rounded-full bg-white text-primary"
+                    >
+                        Späť
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default Modal;
+export default Modal
